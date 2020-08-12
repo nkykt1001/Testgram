9f1c7e70bbbc09e0ad86b0b0b50be9db46e4bb72
nkykt1001 <>
2020-08-12 13:39:51 +0900

    プロフィールページの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_014237) do
+ActiveRecord::Schema.define(version: 2020_08_12_040848) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "accountname"
+    t.text "pr"
+    t.integer "gender"
+    t.date "birthday"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
