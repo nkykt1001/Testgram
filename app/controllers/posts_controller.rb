79bad2a58f0c095c46c26ba8e571c081d4a47a74
nkykt1001 <>
2020-08-13 10:46:26 +0900

    CRUD機能の補完

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
     def index
+        @posts = Post.all
+    end
+
+    def show
+        @post = Post.find(params[:id])
     end
 
     def new
@@ -14,6 +19,26 @@ class PostsController < ApplicationController
             flash.now[:error] = '保存出来ませんでした'
             render :new
         end
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to root_path, notice: '更新出来ました'
+        else
+            flash.now[:error] = '更新出来ませんでした'
+            render :edit
+        end
+    end
+
+    def destroy
+        post = Post.find(params[:id])
+        post.destroy!
+        redirect_to root_path, notice: '削除しました'
     end
 
     private
