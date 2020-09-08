87c35e83c6436ab043c3807c80a0a55fdd024edc
nkykt1001 <>
2020-09-08 09:13:31 +0900

    ajaxによる「いいね」機能の実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,18 @@ class LikesController < ApplicationController
     before_action :authenticate_user!
 
     def create
-        post = Post.find(params[:post_id])
-        post.likes.create!(user_id: current_user.id)
+        @post = Post.find(params[:post_id])
+        @like = Like.create(user_id: current_user.id, post_id: params[:post_id])
+        @likes = Like.where(post_id: params[:post_id])
+        @post.reload
     end
 
     def destroy
-        post = Post.find(params[:post_id])
-        like = post.likes.find_by!(user_id: current_user.id)
-        like.destroy!
+        @post = Post.find(params[:post_id])
+        like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+        like.destroy
+        @likes = Like.where(post_id: params[:post_id])
+        @post.reload
     end
 
 end
