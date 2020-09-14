ee1dc36ee1c8909c462c2b087db01b2e7d6af63f
nkykt1001 <>
2020-09-14 13:58:45 +0900

    uploaderファイルの削除

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Testgram
       Bundler.require(*Rails.groups)
       Dotenv::Railtie.load
     end
-
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
